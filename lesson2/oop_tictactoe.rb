632216342e95fd23241659d9a46fbae7d088f5ac
mozart reina <mozart@mozartreina.com>
2015-10-12 23:12:17 +0900

    Implement Square class & extract Board from TicTacToe

--- lesson2/oop_tictactoe.rb
+++ lesson2/oop_tictactoe.rb
@@ -1,90 +1,99 @@
 class Player
 
-  attr_accessor :score, :name
+  attr_accessor :score, :name, :marker
 
-  def initialize(name)
+  def initialize(name, marker)
     self.name = name
     self.score = 0
+    self.marker = marker
   end
 
 end
 
-class Human < Player
+class Square
 
-  def place_piece(board_pos)
-    begin
-      begin
-        puts "Choose a position from 1 to 9: "
-        pos = gets.chomp.to_i
-      end until (1..9).to_a.include?(pos)
-    end until board_pos[pos - 1] == ' ' # input domain is 1 - 9 but board_pos
-    pos - 1                             # index is 0 -8 thus input - 1
+  attr_accessor :value
+
+  def initialize
+    self.value = ' '
   end
 
 end
 
-class Computer < Player
+class Board
 
-  def place_piece(board_pos)
-    begin
-      pos = (0..8).to_a.sample
-    end until board_pos[pos] == ' '
-    pos
+  attr_accessor :board_pos
+
+  def initialize
+    self.board_pos = {}
+    (1..9).to_a.each {|pos| self.board_pos[pos] = Square.new}
+  end
+
+  def all_marked?
+    empty_squares.length == 0
+  end
+
+  def empty_squares
+    self.board_pos.select { |_, square| square.value == ' '}
+  end
+
+  def mark_square(pos, marker)
+    self.board_pos[pos].value = marker
+  end
+
+  def draw_board
+    system "clear"
+    puts "     |     |     "
+    puts "  #{self.board_pos[1].value}  |  #{self.board_pos[2].value}  |  #{self.board_pos[3].value}  "
+    puts "     |     |     "
+    puts "-----+-----+-----"
+    puts "     |     |     "
+    puts "  #{self.board_pos[4].value}  |  #{self.board_pos[5].value}  |  #{self.board_pos[6].value}  "
+    puts "     |     |     "
+    puts "-----+-----+-----"
+    puts "     |     |     "
+    puts "  #{self.board_pos[7].value}  |  #{self.board_pos[8].value}  |  #{self.board_pos[9].value}  "
+    puts "     |     |     "
+    puts ""
   end
 
 end
 
 class TicTacToe
 
-  attr_accessor :board_pos
-
   def initialize
-    @player = Human.new("Some random guy")
-    @computer = Computer.new("Computer")
+    @human = Player.new("Mo", "X")
+    @computer = Player.new("Computer", "O")
   end
 
   def new_board
-    self.board_pos = []
-    (0..8).each { |pos| self.board_pos[pos] = ' ' }
+    @board = Board.new
   end
 
-  def get_name
-    puts "Please enter your name: "
-    player_name = gets.chomp
-  end
-
-  def draw_board
-    system "clear"
-    puts "     |     |     "
-    puts "  #{board_pos[0]}  |  #{board_pos[1]}  |  #{board_pos[2]}  "
-    puts "     |     |     "
-    puts "-----+-----+-----"
-    puts "     |     |     "
-    puts "  #{board_pos[3]}  |  #{board_pos[4]}  |  #{board_pos[5]}  "
-    puts "     |     |     "
-    puts "-----+-----+-----"
-    puts "     |     |     "
-    puts "  #{board_pos[6]}  |  #{board_pos[7]}  |  #{board_pos[8]}  "
-    puts "     |     |     "
-    puts ""
-    puts "Computer: #{@computer.score}        #{@player.name}: #{@player.score}"
-  end
-
-  def board_full?
-    if self.board_pos.find { |x| x == " " }
-      return nil
-    else
-      "It's a tie"
-    end
+  def play
+    begin
+      new_board
+      @current_player = @human
+      @board.draw_board
+      begin
+        puts "Computer: #{@computer.score}        #{@human.name}: #{@human.score}"
+        current_player_turn
+        @board.draw_board
+        winnerp = check_winner
+      end until winnerp || @board.all_marked?
+      puts winnerp ? winnerp : "It's a tie!"
+      puts "Play again? (y/n)"
+      play_again = gets.chomp
+    end until play_again.downcase == 'n'
   end
 
   def check_winner
-    winning_pos = [[0,1,2], [3,4,5,], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    winning_pos = [[1,2,3], [4,5,6], [7,8,9], [1, 4, 7], [2,5,8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
     winning_pos.each do |pos|
-      if pos.map { |x| board_pos[x] }.all? { |x| x == "X" }
-        @player.score += 1
-        return "#{@player.name} wins!"
-      elsif pos.map { |x| board_pos[x] }.all? { |x| x == "O" }
+      if pos.map { |x| @board.board_pos[x].value }.all? { |x| x == @human.marker }
+        @human.score += 1
+        return "#{@human.name} wins!"
+      elsif pos.map { |x| @board.board_pos[x].value }.all? { |x| x == @computer.marker }
         @computer.score += 1
         return "#{@computer.name} wins!"
       end
@@ -92,35 +101,22 @@ class TicTacToe
     return nil
   end
 
-  def player_turn
-    board_pos[@player.place_piece(board_pos)] = "X"
-  end
-
-  def computer_turn
-    board_pos[@computer.place_piece(board_pos)] = "O"
-  end
-
-  def play
-    @player.name = get_name
-    begin
-      new_board
+  def current_player_turn
+    if @current_player == @human
       begin
-        draw_board
-        player_turn
-        who_won = check_winner
-        break if who_won != nil || board_full? != nil
-        computer_turn
-        who_won = check_winner
-      end until who_won != nil
-      result = board_full? ? board_full? : who_won
-      draw_board
-      puts result
-      puts "Play again? (y/n)"
-      play_again = gets.chomp.downcase
-    end until play_again == 'n'
+        puts "Select a position from 1 to 9: "
+        begin
+          pos = gets.chomp
+        end until @board.empty_squares.keys.include?(pos.to_i)
+        @board.mark_square(pos.to_i, @human.marker)
+        @current_player = @computer
+      end
+    else
+      @board.mark_square(@board.empty_squares.keys.sample, @computer.marker)
+      @current_player = @human
+    end
   end
 
 end
 
-game = TicTacToe.new
-game.play
+TicTacToe.new.play
